365fa8f4d772ee50326c5e754d105335654a1c03
Baillie O'Grady <baillieogrady@Baillies-iMac.local>
2020-06-21 20:41:05 +0100

    Initial

---
+++ Assets/Scripts/followPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Transform player;
+    // store three numbers (floats)
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // when lowercase 't' it refers to the transform of our current object
+        // the object that this script it sitting on
+        transform.position = player.position + offset;
+    }
+}
